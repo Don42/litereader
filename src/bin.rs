28a3ca2fd381cd6cec15f402e2edb481e9c1b713
Marco 'don' Kaulea <DonMarco42@gmail.com>
2017-02-25 20:41:50 +0100

    Check cl arguments before using them
    Command line parsing isn't really implemented, but this is a simple
    panic to fix.

--- src/bin.rs
+++ src/bin.rs
@@ -6,6 +6,10 @@ use liblitereader::{Parser, SqliteFile};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("No file specified");
+        return
+    }
     let file = SqliteFile::from_file(&args[1]);
     println!("{}", file.unwrap());
 }
